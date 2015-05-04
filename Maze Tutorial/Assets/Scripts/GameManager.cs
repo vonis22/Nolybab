dc20cbc2c64f5b9c72b0b8f28e7e32ef38c7d103
vonis22 <vincentvanbeek@home.nl>
2015-05-04 16:59:13 +0200

    FPS Controller Added

--- Maze Tutorial/Assets/Scripts/GameManager.cs
+++ Maze Tutorial/Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	private void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)) {
+		if (Input.GetKeyDown(KeyCode.F1)) {
 			RestartGame();
 		}
 	}
