9e437646e275e1f8040e12b21125aa0bb52be667
vonis22 <vincentvanbeek@home.nl>
2015-05-19 19:32:09 +0200

    DeurBugFixing en sound

--- Maze Tutorial/Assets/Scripts/BreakScript.cs
+++ Maze Tutorial/Assets/Scripts/BreakScript.cs
@@ -11,16 +11,37 @@ public class BreakScript : MonoBehaviour {
 	public int damage = 1;
 	private int roundMineTimerRead = 1;
 
+	//AudioClips
+	public AudioClip breakSound_0;
+	public AudioClip breakSound_1;
+	public AudioClip breakSound_2;
+	public AudioClip breakSound_3;
+	public AudioClip breakSound_4;
+	public AudioClip breakSound_5;
+	public AudioClip breakSound_6;
+	public AudioClip breakSound_7;
+
+
+	void Start ()
+	{
+	}
 	void Update () 
 	{
+
 		textMesh1.GetComponent<TextMesh> ().text = hp.ToString();
 		//textMesh2.text = hp.ToString();
 		if (hp <= 0)
 		{
+			StartCoroutine (crumbleKill ());
 			//Brokkel af animatie
-			Destroy(transform.parent.gameObject);
 		}
+
 		//print (roundMineTimerRead);
+	}
+	IEnumerator crumbleKill()
+	{
+		yield return new WaitForSeconds (2);
+		Destroy(transform.parent.gameObject);
 	}
 
 	void OnTriggerEnter (Collider c)
@@ -30,6 +51,8 @@ public class BreakScript : MonoBehaviour {
 			col1 = GameObject.FindGameObjectWithTag("Pickaxe");
 			print("hak");
 			hp -= damage;
+			GetComponent<AudioSource> ().clip = breakSound_0;
+			GetComponent<AudioSource>().Play();
 			//PlaySound
 			//PlayParticle Animation
 
