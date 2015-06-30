6c3ad46714995f7f04f5cc1b7d2ff8e88e4dc146
Elysiano <patrick.volleybal@hotmail.com>
2015-06-30 14:54:03 +0200

    GIF toegevoegd

---
+++ Nolybab/Assets/Scripts/NolybabGIFAnimation.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NolybabGIFAnimation : MonoBehaviour {
+
+	public GameObject Noly_0;
+	public GameObject Noly_1;
+	public GameObject Noly_2;
+	public GameObject Noly_3;
+	public GameObject Noly_4;
+
+
+	void Start () 
+	{
+		StartCoroutine (animationTimer ());
+	}
+
+	IEnumerator animationTimer()
+	{
+		Noly_0.SetActive (true);
+		Noly_1.SetActive (false);
+		Noly_2.SetActive (false);
+		Noly_3.SetActive (false);
+		Noly_4.SetActive (false);
+		yield return new WaitForSeconds (0.1f);
+		Noly_0.SetActive (false);
+		Noly_1.SetActive (true);
+		Noly_2.SetActive (false);
+		Noly_3.SetActive (false);
+		Noly_4.SetActive (false);
+		yield return new WaitForSeconds (0.1f);		
+		Noly_0.SetActive (false);
+		Noly_1.SetActive (false);
+		Noly_2.SetActive (true);
+		Noly_3.SetActive (false);
+		Noly_4.SetActive (false);
+		yield return new WaitForSeconds (0.1f);		
+		Noly_0.SetActive (false);
+		Noly_1.SetActive (false);
+		Noly_2.SetActive (false);
+		Noly_3.SetActive (true);
+		Noly_4.SetActive (false);
+		yield return new WaitForSeconds (0.1f);		
+		Noly_0.SetActive (false);
+		Noly_1.SetActive (false);
+		Noly_2.SetActive (false);
+		Noly_3.SetActive (false);
+		Noly_4.SetActive (true);
+		yield return new WaitForSeconds (0.1f);
+		StartCoroutine (animationTimer ());
+	}
+}
---
+++ Nolybab/Assets/Scripts/ThomasGIFAnimation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThomasGIFAnimation : MonoBehaviour {
+
+	public GameObject ThomasStillFab;
+	public GameObject ThomasRun_1Fab;
+	public GameObject ThomasRun_2Fab;
+
+	void Start () 
+	{
+		StartCoroutine (animationTimer ());
+	}
+
+	IEnumerator animationTimer()
+	{
+			ThomasStillFab.SetActive (true);
+			ThomasRun_1Fab.SetActive (false);
+			ThomasRun_2Fab.SetActive (false);
+			yield return new WaitForSeconds (0.1f);
+			ThomasStillFab.SetActive (false);
+			ThomasRun_1Fab.SetActive (true);
+			ThomasRun_2Fab.SetActive (false);
+			yield return new WaitForSeconds (0.1f);
+			ThomasStillFab.SetActive (false);
+			ThomasRun_1Fab.SetActive (false);
+			ThomasRun_2Fab.SetActive (true);
+			yield return new WaitForSeconds (0.1f);
+			StartCoroutine (animationTimer ());
+	}
+}
