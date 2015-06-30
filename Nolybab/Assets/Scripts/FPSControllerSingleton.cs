50e8ff8d157a708e0f1a1cd988e73dbe0fbed1fb
BryanKlunder <bryanklunder@hotmail.com>
2015-06-30 22:11:49 +0200

    SingletonFixed

---
+++ Nolybab/Assets/Scripts/FPSControllerSingleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSControllerSingleton : MonoBehaviour
+{
+	private static FPSControllerSingleton _instance;
+	
+	void Awake()
+	{
+		//if we don't have an [_instance] set yet
+		if (!_instance)
+		{
+			_instance = this;
+			this.name = "FPSController1";
+			
+		}
+		//otherwise, if we do, kill this thing
+		else
+		{
+			Destroy(GameObject.Find ("FPSController"));
+		}
+		
+		DontDestroyOnLoad(this.gameObject) ;
+	}
+}
---
+++ Nolybab/Assets/Scripts/GameOptionsSingleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOptionsSingleton : MonoBehaviour
+{
+	private static GameOptionsSingleton _instance;
+	
+	void Awake()
+	{
+		//if we don't have an [_instance] set yet
+		if (!_instance)
+		{
+			_instance = this;
+			this.name = "Options1";
+			
+		}
+		//otherwise, if we do, kill this thing
+		else
+		{
+			Destroy(GameObject.Find ("Options"));
+		}
+		
+		DontDestroyOnLoad(this.gameObject) ;
+	}
+}
