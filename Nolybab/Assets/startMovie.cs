fd3bdb5fcabd5c9b4c25961a6bff5c0e63291898
Elysiano <patrick.volleybal@hotmail.com>
2015-07-01 17:04:25 +0200

    Movie werkend krijgen WIP

---
+++ Nolybab/Assets/startMovie.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class startMovie : MonoBehaviour {
+
+	private MovieTexture introMovie;
+	private AudioSource sound;
+	
+	void Start () 
+	{
+		//introMovie = gameObject.GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		//introMovie.Play ();
+		//(MovieTexture)GetComponent<Image>().material.mainTexture).Play();
+
+		introMovie = (MovieTexture)GetComponent<RawImage> ().texture;
+		introMovie.Play ();
+		sound = GetComponent<AudioSource> ();
+		sound.clip = introMovie.audioClip;
+		sound.Play ();
+	}
+
+	void Update () 
+	{
+//	 	if (!introMovie.isPlaying)
+//		{
+//			print ("movie playing status: " + introMovie.isPlaying + ".");
+//			introMovie.Play ();
+//		}
+	}
+}
